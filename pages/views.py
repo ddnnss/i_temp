16fa07dfd0ec273fbd453067b4555acb923846d2
xxx <11@11.11>
2020-11-02 19:49:14 +0300

    new pages

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+def about(request):
+   index = True
+   about = True
+   return render(request, 'pages/about.html', locals())
+
 def index(request):
    index=True
    return render(request, 'pages/index.html', locals())
